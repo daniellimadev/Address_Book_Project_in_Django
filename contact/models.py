a5705edf7b9f5510c2a768d86fdbc1f9730d2443
Daniel Lima <pdaniel643@gmail.com>
2024-02-17 17:33:49 -0300

    ForeignKey - creating the Model Category and linking it to the foreign key and owner
    Signed-off-by: Daniel Lima <pdaniel643@gmail.com>

--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self) -> str:
+        return self.name
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -11,6 +18,16 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
     
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
