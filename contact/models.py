65ab074346b8f159dc1974a14bbe99f67d56daf3
Daniel Lima <pdaniel643@gmail.com>
2024-02-18 11:08:39 -0300

    Creating the table that displays contacts in index.html
    Signed-off-by: Daniel Lima <pdaniel643@gmail.com>

--- contact/models.py
+++ contact/models.py
@@ -1,13 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
 
-# Create your models here.
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
-    
+
     def __str__(self) -> str:
         return self.name
+
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -17,7 +21,7 @@ class Contact(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
-    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
@@ -28,6 +32,6 @@ class Contact(models.Model):
         on_delete=models.SET_NULL,
         blank=True, null=True
     )
-    
+
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
